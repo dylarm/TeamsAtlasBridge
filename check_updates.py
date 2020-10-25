3d4f47bd2efecb97dcbc98f931ccafc7c485df2a
Dylan Armitage <dylanjarmitage@gmail.com>
2020-10-25 13:58:47 -0700

    Add check_updates.py and basic version getting
    No version checking/comparison just yet.

---
+++ check_updates.py
@@ -0,0 +1,26 @@
+import logging
+import requests
+from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
+
+from constants import VERSION as CURRENT_VERSION
+
+logger = logging.getLogger(__name__)
+
+
+def get_latest_ver(
+    url: str = "https://api.github.com/repos/dylarm/TeamsAtlasBridge/releases/latest"
+) -> str:
+    latest_version: str = ""
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # For any HTTP Errors
+        latest_version = response.json()["tag_name"]
+    except HTTPError as e:
+        logger.warning(f"A HTTP Status error occurred while checking for updates:\n{e}")
+    except (ConnectionError, Timeout) as e:
+        logger.warning(f"A Connection error occurred while checking for updates:\n{e}")
+    except RequestException as e:
+        logger.warning(
+            f"A potentially serious error occurred while checking for updates:\n{e}"
+        )
+    return latest_version
