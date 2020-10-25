ffb88de3736ba7c41aaf21b47b85ad0359207812
Dylan Armitage <dylanjarmitage@gmail.com>
2020-10-24 20:13:34 -0700

    Add logging ability - process.py
    Logging added to process.py. Hopefully it's not a pain to maintain.

--- process.py
+++ process.py
@@ -1,8 +1,11 @@
+import logging
 from pathlib import Path
 
 import pandas as pd
 
 import constants as CONSTANT
+
+logger = logging.getLogger(__name__)
 
 
 def assignment_name(file: Path) -> str:
@@ -12,9 +15,12 @@ def assignment_name(file: Path) -> str:
     :param file: Path
     :return: str
     """
+    logger.debug("Processing assignment name...")
     with open(file, "r") as f:
         line = f.readline()
-    return line.split(",")[3].strip('"')
+    name = line.split(",")[3].strip('"')
+    logger.info(f"Assignment name: {name}")
+    return name
 
 
 def class_period(file: Path) -> int:
@@ -24,8 +30,11 @@ def class_period(file: Path) -> int:
     :param file: Path
     :return: int
     """
+    logger.debug("Extracting class period...")
     loc = file.name.find("P0")
-    return int(file.name[loc + 2])
+    period = int(file.name[loc + 2])
+    logger.info(f"Class period: {period}")
+    return period
 
 
 def assignment_file_name(assignment: str, period: int) -> str:
@@ -36,7 +45,10 @@ def assignment_file_name(assignment: str, period: int) -> str:
     :param period: int
     :return: str
     """
-    return f"{period} - {assignment}"
+    logger.debug("Creating assignment file name for export...")
+    name = f"{period} - {assignment}"
+    logger.info(f"Output file name: {name}")
+    return name
 
 
 def generate_output(assignment_file: Path, student_list: Path, output: Path) -> None:
@@ -48,12 +60,18 @@ def generate_output(assignment_file: Path, student_list: Path, output: Path) -> 
     :param output: Path
     :return: None
     """
+    logger.info("Generating matched file...")
     students = pd.read_excel(student_list, **CONSTANT.STUDENT_LOGINS)
+    logger.debug("Student login file loaded")
     teams = pd.read_csv(assignment_file, **CONSTANT.TEAMS_CSV)
+    logger.debug("Teams grade CSV file loaded")
+    logger.info("Input files loaded, matching email addresses...")
     matched_file = teams.merge(
         students,
         how="left",
         left_on=teams.columns[0],
         right_on=CONSTANT.STUDENT_LOGINS["usecols"][1],
     )
+    logger.info("Email addresses matched. Writing file(s) out")
     matched_file.to_excel(output, index=False)
+    logger.info("File(s) finished writing")
