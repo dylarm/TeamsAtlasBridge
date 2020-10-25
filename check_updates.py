c53ef673bc6207f948f8af72968f91cffafa4309
Dylan Armitage <dylanjarmitage@gmail.com>
2020-10-25 14:10:21 -0700

    Add basic update checking
    Using requests, get the latest version tag. If it's greater than the current version, then... do something. That'll be up to the GUI.

--- check_updates.py
+++ check_updates.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 def get_latest_ver(
     url: str = "https://api.github.com/repos/dylarm/TeamsAtlasBridge/releases/latest"
 ) -> str:
-    latest_version: str = ""
+    latest_version: str = "z"  # Will always be the "greatest" version possible
     try:
         response = requests.get(url)
         response.raise_for_status()  # For any HTTP Errors
@@ -24,3 +24,9 @@ def get_latest_ver(
             f"A potentially serious error occurred while checking for updates:\n{e}"
         )
     return latest_version
+
+
+def update_available(
+    current_ver: str = CURRENT_VERSION, latest_ver: str = get_latest_ver()
+) -> bool:
+    return latest_ver > current_ver
